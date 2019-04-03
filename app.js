93c318dd79433b7222bbb99137018305ecee8bd4
Nate <natethepcspecialist@gmail.com>
2019-04-03 15:25:24 -0400

    update

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express"),
-  pug = requrie("pug"),
+  pug = require("pug"),
   app = express();
 
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/public/views");
+app.get("/", (req, res) => res.render("index"));
 app.listen(process.env.PORT || 3000);
